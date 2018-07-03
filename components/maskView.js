5aab7560e7973f8eac77a2ee2a749d11f2e79f0b
youj <282295491@11.com>
2018-07-03 17:44:23 +0800

    蒙层（不覆盖navigationBar）

--- components/maskView.js
+++ components/maskView.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import { 
-  View
+  View,
+  Dimensions
 } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
 
 export default class MaskView extends Component {
 
     render() {
         return (
-            <View/>
+            <View style={{ width, height, position: 'absolute', backgroundColor: 'red', opacity: 0.3 }}/>
         );
     }
 }
--- screens/home/maskViewSample.js
+++ screens/home/maskViewSample.js
@@ -1,7 +1,11 @@
 import React, {Component} from 'react';
 import {
     View,
+    StyleSheet
 } from 'react-native';
+
+import RNButton from '../../components/customButton'
+import MaskView from '../../components/maskView'
 
 export default class MaskViewSample extends Component {
 
@@ -9,9 +13,46 @@ export default class MaskViewSample extends Component {
         title: '蒙层',//对页面的配置
       };
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            showMaskView: false,
+        }
+    };
+
     render() {
         return (
-            <View/>
+            <View style={styles.container}>
+                <RNButton 
+                    style={styles.button}
+                    title="蒙层（不覆盖NavigationBar）" 
+                    titleStyle={{color: 'white'}}
+                    onPress={()=>{this.setState({showMaskView:true});}}
+                />
+                {this.state.showMaskView && (
+                    <MaskView/>
+                )}
+            </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        backgroundColor: '#F5FCFF',
+    },
+    button: {
+        flex: 1,
+        top: 30,
+        marginLeft: 50,
+        marginRight: 50,
+        height: 45,
+        borderRadius: 5,
+        backgroundColor: '#3097FD',
+    }
+});
